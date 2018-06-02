0e7383512a3f998f5a06df68964d9daa0587037b
jeremyplack <stlouisweb@gmail.com>
2018-06-02 09:07:06 -0500

    refactor the SRM method to calculate the weighted ave. malt color before accounting for batch size (results in few divison operations).

--- src/index.js
+++ src/index.js
@@ -14,11 +14,17 @@
  * @returns {number} SRM - The SRM color estimate for recipe.
  */
 exports.SRM = (malts: { L: number, Lbs: number }[], batchSize: number) => {
+  // mC = maltColor, the weighted average Lovibond of all the malt in a recipe.
+  const mC = malts.reduce(
+    (total: number, malt: { L: number, Lbs: number }) =>
+      total + malt.L * malt.Lbs,
+    0
+  );
+
   // MCU = Malt Color Units, the color of each grain * the grain weight in Lbs
   // divided by the batch volume in gal.
-  const MCU = malts.map(
-    (malt: { L: number, Lbs: number }) => (malt.L * malt.Lbs) / batchSize
-  );
+  const MCU = mC / batchSize;
+
   // Since light absorbance is non-linear we use the Morey equation below to
   // calculate SRM.
   const SRM = 1.49 * (MCU * 0.69);
